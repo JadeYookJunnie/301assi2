0fd548674cd325c4764e6619f027c0db2c8e4a68
JadeYookJunnie <105710713+JadeYookJunnie@users.noreply.github.com>
2023-04-14 17:40:17 +1200

    Add files via upload

--- LZencode.java
+++ LZencode.java
@@ -1,2 +1,28 @@
+import java.io.*;
 public class LZencode {
+    public static void main(String[] args) {
+
+
+
+        try{
+            int pointer = 1;
+            byte[] argument = args[0].getBytes();
+            InputStream isr = new ByteArrayInputStream(argument);
+            byte[] byteArray = isr.readAllBytes();
+            translator(byteArray);
+        }
+        catch(Exception e){
+            e.getStackTrace();
+        }
+
+    }
+    //byte to hex translator
+    public static void translator(byte[] bytes){
+        for(byte b: bytes) {
+            String st = String.format("%02X", b);
+            System.out.println(st);
+        }
+
+    }
+
 }
