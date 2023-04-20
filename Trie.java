9817d8b9aff82d366f7871ad2fdbd3e663a4bb74
sy1122young <90888243+sy1122young@users.noreply.github.com>
2023-04-20 14:29:14 +1200

    Add files via upload

--- Trie.java
+++ Trie.java
@@ -1,12 +1,13 @@
-public class Trie{
-    
-    //0-9 a-f  14
+public class Trie {
+
+    // 0-9 a-f 14
     static final int SYMBOL_SIZE = 26;
-    //array of hex values
+    // array of hex values
     public static String[] keys;
+
     public Trie(String[] _input) {
         keys = _input;
-        //System.out.println("done");
+        // System.out.println("done");
         main(null);
     }
 
@@ -26,66 +27,69 @@ public class Trie{
         ;
     }
 
-        public static void main(String args[])
-        {
-            System.out.println("main");
-            // Input keys (use only 'a' through 'z' and lower case)
-            //conversion error
-//            String keys[] = {"the", "a", "there", "answer", "any",
-//                    "by", "bye", "their"};
+    public static void main(String args[]) {
+        System.out.println("main");
+        // Input keys (use only 'a' through 'z' and lower case)
+        // conversion error
+        // String keys[] = {"hello"};
 
-            String output[] = {"Not present in trie", "Present in trie"};
+        String output[] = { "Not present in trie", "Present in trie" };
 
-            root = new TrieNode();
+        root = new TrieNode();
 
-            // Construct trie
-            //from here
-            //System.out.println(keys.length);
-            //only called once
-            int i;
-            for (i = 0; i < keys.length ; i++) {
-                System.out.println(keys[i]);
-                insert(keys[i]);
-            }
+        // Construct trie
+        // from here
+        // System.out.println(keys.length);
+        // only called once
 
-
-
-            // Search for different keys
-            if(search("68") == true)
-                System.out.println("the --- " + output[1]);
-            else System.out.println("the --- " + output[0]);
-//
-//            if(search("these") == true)
-//                System.out.println("these --- " + output[1]);
-//            else System.out.println("these --- " + output[0]);
-//
-//            if(search("their") == true)
-//                System.out.println("their --- " + output[1]);
-//            else System.out.println("their --- " + output[0]);
-//
-//            if(search("thaw") == true)
-//                System.out.println("thaw --- " + output[1]);
-//            else System.out.println("thaw --- " + output[0]);
-
+        System.out.println("this is key lengh" + keys.length);
+        for (int i = 0; i < keys.length; i++) {
+            System.out.println(keys[i]);
+            insert(keys[i]);
         }
 
-        static TrieNode root;
-        // If not present, inserts key into trie
-        // If the key is prefix of trie node,
-        // just marks leaf node
+        // Search for different keys
+        if (search("68") == true)
+            System.out.println("the --- " + output[1]);
+        else
+            System.out.println("the --- " + output[0]);
+        //
+        // if(search("these") == true)
+        // System.out.println("these --- " + output[1]);
+        // else System.out.println("these --- " + output[0]);
+        //
+        // if(search("their") == true)
+        // System.out.println("their --- " + output[1]);
+        // else System.out.println("their --- " + output[0]);
+        //
+        // if(search("thaw") == true)
+        // System.out.println("thaw --- " + output[1]);
+        // else System.out.println("thaw --- " + output[0]);
 
-    static void insert(String key)
-    {
+    }
+
+    static TrieNode root;
+    // If not present, inserts key into trie
+    // If the key is prefix of trie node,
+    // just marks leaf node
+
+    static void insert(String key) {
         System.out.println("insert");
-        int level;
-        int length = key.length();
+
         int index;
 
         TrieNode pCrawl = root;
-
-        for (level = 0; level < length; level++)
-        {
-            index = key.charAt(level) - 'a';
+        System.out.println("we made it to for loop");
+        for (int level = 0; level < key.length(); level++) {
+            // get the 2 numbers
+            char one = key.charAt(level);
+            char two = key.charAt((level + 1));
+            // concatnate them into a single value
+            String letter = String.valueOf(one) + two;
+            System.out.println("letter" + letter);
+            // minus the ascii value
+            index = Integer.parseInt(letter) - 61;
+            System.out.println("index" + index);
             if (pCrawl.children[index] == null)
                 pCrawl.children[index] = new TrieNode();
 
@@ -95,23 +99,26 @@ public class Trie{
         // mark last node as leaf
         pCrawl.isEndOfWord = true;
     }
-        //returns true if key presents in trie, else false
-        static boolean search(String key){
-            int level;
-            int length = key.length();
-            int index;
-            TrieNode pCrawl = root;
 
-            for(level = 0; level < length; level++){
-                index = key.charAt(level) - 'a';
-                if(pCrawl.children[index] == null)
-                    return false;
-                pCrawl = pCrawl.children[index];
-            }
-            return (pCrawl.isEndOfWord);
+    // returns true if key presents in trie, else false
+    static boolean search(String key) {
+        int level;
+        int length = key.length();
+        int index;
+        TrieNode pCrawl = root;
+
+        for (level = 0; level < length; level++) {
+
+            char one = key.charAt(level);
+            char two = key.charAt((level + 1));
+            String letter = String.valueOf(one) + two;
+            index = Integer.parseInt(letter) - 61;
+            if (pCrawl.children[index] == null)
+                return false;
+            pCrawl = pCrawl.children[index];
         }
-        // Driver
-
-
+        return (pCrawl.isEndOfWord);
+    }
+    // Driver
 
 }
